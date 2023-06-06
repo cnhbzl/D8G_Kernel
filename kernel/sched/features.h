e28100e75f4b49b8ddb16b8235b31cf6088f6346
diphons <diphons@gmail.com>
2023-06-06 15:21:47 +0700

    sched_feat: Add schedtune boost util

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -111,6 +111,12 @@ SCHED_FEAT(FBT_STRICT_ORDER, false)
  */
 SCHED_FEAT(EAS_USE_NEED_IDLE, true)
 
+/*
+ * Inflate the effective utilization of SchedTune-boosted tasks, which
+ * generally leads to usage of higher frequencies.
+ * If disabled, boosts will only bias tasks to higher-capacity CPUs.
+ */
+SCHED_FEAT(SCHEDTUNE_BOOST_UTIL, true)
 
 /*
  * Apply schedtune boost hold to tasks of all sched classes.
