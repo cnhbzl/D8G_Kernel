c350050a5f2d91c08bb13b6535ddc5119447250d
diphons <diphons@gmail.com>
2023-06-06 14:35:58 +0700

    Inline with oprofile

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -26,6 +26,7 @@
 #include <linux/printk.h>
 #include <linux/hrtimer.h>
 #include <linux/of.h>
+#include <misc/d8g_helper.h>
 #include "governor.h"
 
 static struct class *devfreq_class;
@@ -1313,7 +1314,12 @@ static int __init devfreq_init(void)
 		return PTR_ERR(devfreq_class);
 	}
 
-	devfreq_wq = create_freezable_workqueue("devfreq_wq");
+	if (oprofile != 4 && oprofile != 0)
+		devfreq_wq = alloc_workqueue("devfreq_wq", WQ_HIGHPRI | WQ_FREEZABLE
+			| WQ_UNBOUND | WQ_MEM_RECLAIM, 1);
+	else
+		devfreq_wq = create_freezable_workqueue("devfreq_wq");
+
 	if (!devfreq_wq) {
 		class_destroy(devfreq_class);
 		pr_err("%s: couldn't create workqueue\n", __FILE__);
